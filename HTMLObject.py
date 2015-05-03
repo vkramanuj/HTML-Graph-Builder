64533de8fca3f776f941427ad897f7f08af8bb0c
Vivek Ramanujan <ramanv@cs.washington.edu>
2015-05-02 19:47:44 -0700

    Updated seperate HTMLObject file, far more complete

--- HTMLObject.py
+++ HTMLObject.py
@@ -1,11 +1,43 @@
-class HTMLObject:
-	"""docstring for HTMLObject"""
+from lxml.html import iterlinks
+import sys
+import urllib
+class HTMLObject(object):
+	"""HTMLObject manages html links, does lazy initialization for everything"""
 	def __init__(self, link):
 		self.link = link
 		self.source = "null"
+		self.links = []
 
-	def get_source(self):
+	def get_source(self, noReturn = False):
 		if self.source == "null":
-			self.source = get_source(link)
+			source = ""
+			try:
+				webPage = urllib.urlopen(self.link)
+				source = webPage.read()
+				webPage.close()
+			except Exception, e:
+				source = "NOTHING"
 
-		return self.source
+			self.source = source
+
+		if not noReturn:
+			return self.source
+
+	def get_links(self, noReturn = False):
+		if self.links == []:
+			so = self.get_source()
+			self.links = list(iterlinks(so))
+		
+		if not noReturn:
+			return self.links
+
+	def update(self, link = None):
+		if link is None:
+			link = self.link
+		
+		self.link = link
+		self.get_source(True)
+		self.get_links(True)
+
+	def link(self):
+		return self.link
--- get_links.py
+++ get_links.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 # webcrawler and spider.add, a*
 import urllib
+import HTMLObject
 from lxml import etree
 
 def get_source(link):
@@ -11,19 +12,6 @@ def get_source(link):
 		return source
 	except Exception, e:
 		return "NOTHING"
-
-class HTMLObject:
-	"""docstring for HTMLObject"""
-	def __init__(self, link):
-		self.link = link
-		self.source = "null"
-
-	def get_source(self):
-		if self.source == "null":
-			self.source = get_source(link)
-
-		return self.source
-
 	
 def parse_with_tree(source):
 	print source 
